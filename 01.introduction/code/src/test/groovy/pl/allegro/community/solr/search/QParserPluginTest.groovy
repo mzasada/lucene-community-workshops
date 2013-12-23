8d77d279b33eb46e45550aeeeeb90df50abd911a
Maciej Zasada <maciej.zasada@allegro.pl>
2013-12-23 21:02:09 +0100

    fix: added maven repository with restlet-releases.

---
+++ 01.introduction/code/src/test/groovy/pl/allegro/community/solr/search/QParserPluginTest.groovy
@@ -0,0 +1,12 @@
+package pl.allegro.community.solr.search
+
+import spock.lang.Specification
+
+class QParserPluginTest extends Specification {
+
+    def "should pass"() {
+
+        expect:
+        1 == 1
+    }
+}
