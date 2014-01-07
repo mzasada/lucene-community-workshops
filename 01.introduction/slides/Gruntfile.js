8fbc3c8dec7050ae0531e9ec7e12fa3088debbd3
mzasada <maciejekz@gmail.com>
2014-01-07 16:31:02 +0100

    feat: Gruntfile config.

--- 01.introduction/slides/Gruntfile.js
+++ 01.introduction/slides/Gruntfile.js
@@ -1,2 +1,27 @@
 module.exports = function (grunt) {
-}
+
+  grunt.initConfig({
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        'package.json'
+      ],
+      options: {
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('www', ['connect:server']);
+
+  // load standard Grunt tasks
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+};
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 lucene-community-workshops
 ==========================
+
+01. Introduction
+
+How to run slides:
+# Change to the 01.introduction/slides directory.
+# Install project dependencies with *npm install*.
+# Run Grunt with *grunt www*.
